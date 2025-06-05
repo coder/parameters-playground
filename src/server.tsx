c67adc00446b3d0499298410a2f5e65b31509090
Brett Kolodny <brett.kolodny@coder.com>
2025-06-05 16:19:05 +0000

    fix: update css improt

--- src/server.tsx
+++ src/server.tsx
@@ -6,40 +6,40 @@ import { handle } from "hono/vercel";
 
 const app = new Hono().basePath("/api");
 
- // app.use(
- // 	"*",
- // 	serveStatic({
- // 		root: "./dist",
- // 		getContent: async (path, _) => {
- // 			try {
- // 				const data = await fs.readFile(path);
- // 				let contentType = "text/plain";
+// app.use(
+// 	"*",
+// 	serveStatic({
+// 		root: "./dist",
+// 		getContent: async (path, _) => {
+// 			try {
+// 				const data = await fs.readFile(path);
+// 				let contentType = "text/plain";
 
- // 				if (path.endsWith(".html")) {
- // 					contentType = "text/html";
- // 				} else if (path.endsWith(".js")) {
- // 					contentType = "application/javascript";
- // 				} else if (path.endsWith(".css")) {
- // 					contentType = "text/css";
- // 				} else if (path.endsWith(".json")) {
- // 					contentType = "application/json";
- // 				} else if (path.endsWith(".png")) {
- // 					contentType = "image/png";
- // 				} else if (path.endsWith(".jpg") || path.endsWith(".jpeg")) {
- // 					contentType = "image/jpeg";
- // 				}
+// 				if (path.endsWith(".html")) {
+// 					contentType = "text/html";
+// 				} else if (path.endsWith(".js")) {
+// 					contentType = "application/javascript";
+// 				} else if (path.endsWith(".css")) {
+// 					contentType = "text/css";
+// 				} else if (path.endsWith(".json")) {
+// 					contentType = "application/json";
+// 				} else if (path.endsWith(".png")) {
+// 					contentType = "image/png";
+// 				} else if (path.endsWith(".jpg") || path.endsWith(".jpeg")) {
+// 					contentType = "image/jpeg";
+// 				}
 
- // 				return new Response(data, {
- // 					headers: {
- // 						"Content-Type": contentType,
- // 					},
- // 				});
- // 			} catch (error) {
- // 				return null;
- // 			}
- // 		},
- // 	}),
- // );
+// 				return new Response(data, {
+// 					headers: {
+// 						"Content-Type": contentType,
+// 					},
+// 				});
+// 			} catch (error) {
+// 				return null;
+// 			}
+// 		},
+// 	}),
+// );
 
 app.get("/", (c) => {
 	return c.json({ message: "Congrats! You've deployed Hono to Vercel" });
@@ -59,7 +59,11 @@ app.get("/foo", (c) => {
 							content="width=device-width, initial-scale=1.0"
 						/>
 						<title>Paramaters Playground</title>
-						<link rel="stylesheet" href="src/index.css" />
+						{import.meta.env.PROD ? (
+							<link rel="stylesheet" href="assets/main.css" />
+						) : (
+							<link rel="stylesheet" href="src/index.css" />
+						)}
 						<script type="module" src="/wasm_exec.js"></script>
 					</head>
 					<body>
